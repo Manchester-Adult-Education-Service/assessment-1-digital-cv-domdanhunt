69cd4c9bf81567127cffa3cf215a9d269a7a15b1
domdanhunt <792336@manadulted.ac.uk>
2026-03-31 20:01:14 +0000

    Saving progress

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
+#alert("Welcome to my portfolio! Feel free to contact me if you have any queries.");
+
+
 const nameHeading = document.querySelector('h1');
 const hour = new Date().getHours();
 console.log("The current hour is: " + hour);
 
-#alert("Welcome to my portfolio! Feel free to contact me if you have any queries.");
-
-// if (hour < 12) {
-//     nameHeading.textContent = "Good Morning, Dominic";
-// } else if (hour < 18) {
-//     nameHeading.textContent = "Good Afternoon, Dominic";
-// } else {
-//     nameHeading.textContent = "Good Evening, Dominic"
-// }
+if (hour < 12) {
+    nameHeading.textContent = "Good Morning!";
+} else if (hour < 18) {
+     nameHeading.textContent = "Good Afternoon!";
+} else {
+    nameHeading.textContent = "Good Evening!"
+}
 
 const year = new Date().getFullYear();
 document.getElementById("copyright").textContent = `Copyright © ${year} Dominic`
 
+
 const time = new Date().toLocaleTimeString();
 document.getElementById('time').textContent = time;
 
-document.getElementById('highlight').style.backgroundColor = blue
+document.getElementById('highlight').style.backgroundColor = blue;
 
 
