561f55356d7c0c8569a76ff50806799b249e48ce
domdanhunt <792336@manadulted.ac.uk>
2026-03-31 17:12:50 +0000

    Made changed to layout with flexbox

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ const nameHeading = document.querySelector('h1');
 const hour = new Date().getHours();
 console.log("The current hour is: " + hour);
 
-alert("Welcome to my portfolio! Feel free to contact me if you have any queries.");
+#alert("Welcome to my portfolio! Feel free to contact me if you have any queries.");
 
-if (hour < 12) {
-    nameHeading.textContent = "Good Morning, Dominic";
-} else if (hour < 18) {
-    nameHeading.textContent = "Good Afternoon, Dominic";
-} else {
-    nameHeading.textContent = "Good Evening, Dominic"
-}
+// if (hour < 12) {
+//     nameHeading.textContent = "Good Morning, Dominic";
+// } else if (hour < 18) {
+//     nameHeading.textContent = "Good Afternoon, Dominic";
+// } else {
+//     nameHeading.textContent = "Good Evening, Dominic"
+// }
 
 const year = new Date().getFullYear();
 document.getElementById("copyright").textContent = `Copyright © ${year} Dominic`
