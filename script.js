076958e7cecc65f530583bf7e8178a1e9845fb15
domdanhunt <792336@manadulted.ac.uk>
2026-04-01 13:38:27 +0000

    Added HTML sections with a view to show/hide in the future

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
 alert("Welcome to my portfolio! Feel free to contact me if you have any queries.");
-const mainContainer = document.querySelector("main");
-const headerBar = document.querySelector("header");
-const subTitles = document.querySelectorAll("h2");
 
-document.getElementById("theme-btn").addEventListener('click', function(document.body.style.backgroundColor === "#eca9a9") {
-    if (document.body.style.backgroundColor === "#eca9a9") {
-        document.body.style.backgroundColor = "#3E5641";
-        document.body.style.color = "#F0F0F0";
-    } else {
-        document.body.style.color = "#eca9a9";
-    }
-});
 
 const nameHeading = document.querySelector('h1');
 const hour = new Date().getHours();
@@ -37,5 +26,18 @@ allSkills.forEach(skill => {
     if (skill.textContent.includes ("Python")) {
         skill.style.color = "#ea580c"; 
         skill.style.fontWeight = "bold";
+    }
+});
+
+const mainContainer = document.querySelector("main");
+const headerBar = document.querySelector("header");
+const subTitles = document.querySelectorAll("h2");
+
+document.getElementById("theme-btn").addEventListener('click', function() {
+    if (document.body.style.backgroundColor === "none") {
+        document.body.style.backgroundColor = "#3E5641";
+        document.body.style.color = "#F0F0F0";
+    } else {
+        document.body.style.color = "#eca9a9";
     }
 });
