2e0b70260984c4771a03d2b044cca0667d6621f1
domdanhunt <792336@manadulted.ac.uk>
2026-04-01 12:14:01 +0000

    Added skill function

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-#alert("Welcome to my portfolio! Feel free to contact me if you have any queries.");
+alert("Welcome to my portfolio! Feel free to contact me if you have any queries.");
+const mainContainer = document.querySelector("main");
+const headerBar = document.querySelector("header");
+const subTitles = document.querySelectorAll("h2");
 
+document.getElementById("theme-btn").addEventListener('click', function(document.body.style.backgroundColor === "#eca9a9") {
+    if (document.body.style.backgroundColor === "#eca9a9") {
+        document.body.style.backgroundColor = "#3E5641";
+        document.body.style.color = "#F0F0F0";
+    } else {
+        document.body.style.color = "#eca9a9";
+    }
+});
 
 const nameHeading = document.querySelector('h1');
 const hour = new Date().getHours();
@@ -20,13 +31,11 @@ document.getElementById("copyright").textContent = `Copyright © ${year} Dominic
 const time = new Date().toLocaleTimeString();
 document.getElementById('time').textContent = time;
 
-document.getElementById('highlight').style.backgroundColor = blue;
+const allSkills = document.querySelectorAll("li");
 
-document.getElementById('theme-btn').addEventListener('click', function() {
-    if (document.body.style.backgroundColor) {
-        document.body.style.backgroundColor = #3E5641;
-        document.body.style.color = #F0F0F0;
-    } else {
-        document.body.style.color = #eca9a9
+allSkills.forEach(skill => {
+    if (skill.textContent.includes ("Python")) {
+        skill.style.color = "#ea580c"; 
+        skill.style.fontWeight = "bold";
     }
 });
